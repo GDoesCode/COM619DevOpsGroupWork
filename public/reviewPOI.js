c6c0943c36a3782d3e2fece8462f71bfd18813f5
mac <5SAJIM26@solent.ac.uk>
2023-11-02 13:45:47 +0000

    Port Numbers Changed for REST calls

--- public/reviewPOI.js
+++ public/reviewPOI.js
@@ -5,7 +5,7 @@ async function submitReview(poiID,reviewIn){
                 poi_id : poiID,
                 review : reviewIn
             }
-            const response1 = await fetch(`http://localhost:3000/poi/review`,{
+            const response1 = await fetch(`http://localhost:8080/poi/review`,{
                 method:"POST",
                 headers:{
                     'Content-Type' : 'application/json'
