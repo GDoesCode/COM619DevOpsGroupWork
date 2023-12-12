b7e373324664c94d5d07ad0dc3ed24c8ecf0d811
Gaetano Archer <5archg31@solent.ac.uk>
2023-12-12 23:24:31 +0000

    swagger setup

---
+++ Swagger.js
@@ -0,0 +1,36 @@
+import SwaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+
+export const initialiseSwagger = (app) => {
+    const configuration = {
+        definition: {
+            openapi: '3.0.0',
+            info: {
+                title: 'NodeNinjas',
+                version: '0.0.1',
+                description: 'NodeNinjas API'
+            },
+            servers: [
+                {
+                    url: 'https://opennms1uksouthcloudazureapp.brazilsouth.cloudapp.azure.com'
+                },
+                {
+                    url: 'http://localhost:8080'
+                }
+            ],
+            components: {
+                securitySchemes: {
+                    bearerAuth: {
+                        type: 'http',
+                        scheme: 'bearer',
+                        bearerFormat: 'JWT'
+                    }
+                }
+            }
+        },
+        apis: ['.routes/poi.mjs']
+    };
+    const specification = swaggerJsDoc(configuration);
+
+    app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(specification));
+};
