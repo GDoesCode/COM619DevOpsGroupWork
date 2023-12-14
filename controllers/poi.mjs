e7f345537a59a6f05954ff5abb3225b158f46480
Gaetano Archer <5archg31@solent.ac.uk>
2023-12-14 15:16:26 +0000

    Added License

--- controllers/poi.mjs
+++ controllers/poi.mjs
@@ -35,118 +35,114 @@ class PoiController{
         }
     }
 
-checkPOIID(req,res){
-    try{
-        this.dao.checkPOIID(req.params.id)
-        .then((poi)=>{
-            res.json(poi);
-        })
-        .catch((error)=>{
-            res.status(500).json({ error: error });
-        });
-    }catch (error){
-        res.status(500).json({ error: error });
-    }
-        const poi = this.dao.checkPOIID(req.params.id)
-        if (poi == true){
-            res.status(200)
-        }
-}
-
-deletePOIById(req, res) {
-    try {
-        console.log(req.params.id)
-        this.dao.deletePOIById(req.params.id)
-            .then(result => {
-                // Handle success, send a response, etc.
-                res.status(200).json({ message: 'POI deleted successfully.\nPlease reload the page to review changes', result });
+    checkPOIID(req,res) {
+        try{
+            this.dao.checkPOIID(req.params.id)
+            .then((poi)=>{
+                res.json(poi);
             })
-            .catch(error => {
-                // Handle errors, send an error response, etc.
-                console.error('You are not logged in, go away!:', error);
-                res.status(500).json({ error: 'Internal Server Error' });
+            .catch((error)=>{
+                res.status(500).json({ error: error });
             });
-    } catch (error) {
-        // Handle synchronous errors (unlikely in an asynchronous context)
-        console.error('Synchronous error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-
-
-
-createPOI(req,res){
-    try{
-        if (req.body.name != ""  && req.body.type != "" && req.body.country != "" && req.body.region != "" && req.body.lat != "" && req.body.lon != "" && req.body.description !=""){
-            if (req.body.name != null  && req.body.type != null && req.body.country != null && req.body.region != null && req.body.lat != null && req.body.lon != null && req.body.description !=null){
-        const poi = this.dao.createPOI(req.body.name, req.body.type,req.body.country,req.body.region,req.body.lon,req.body.lat,req.body.description,req.body.recommendations)
-        res.status(200).json({success:1});
+        }catch (error){
+            res.status(500).json({ error: error });
+        }
+            const poi = this.dao.checkPOIID(req.params.id)
+            if (poi == true){
+                res.status(200)
             }
+    }
+
+    deletePOIById(req, res) {
+        try {
+            console.log(req.params.id)
+            this.dao.deletePOIById(req.params.id)
+                .then(result => {
+                    // Handle success, send a response, etc.
+                    res.status(200).json({ message: 'POI deleted successfully.\nPlease reload the page to review changes', result });
+                })
+                .catch(error => {
+                    // Handle errors, send an error response, etc.
+                    console.error('You are not logged in, go away!:', error);
+                    res.status(500).json({ error: 'Internal Server Error' });
+                });
+        } catch (error) {
+            // Handle synchronous errors (unlikely in an asynchronous context)
+            console.error('Synchronous error:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    createPOI(req,res) {
+        try{
+            if (req.body.name != ""  && req.body.type != "" && req.body.country != "" && req.body.region != "" && req.body.lat != "" && req.body.lon != "" && req.body.description !=""){
+                if (req.body.name != null  && req.body.type != null && req.body.country != null && req.body.region != null && req.body.lat != null && req.body.lon != null && req.body.description !=null){
+            const poi = this.dao.createPOI(req.body.name, req.body.type,req.body.country,req.body.region,req.body.lon,req.body.lat,req.body.description,req.body.recommendations)
+            res.status(200).json({success:1});
+                }
+                else{
+                    res.status(400).json({error: req})
+                }
+            } else{
+                res.status(400).json({error: "Blank Field"})
+            }
+        } catch(error){
+            throw error;
+        }
+    }
+
+    editPOI(req,res) {
+        try{
+            if (req.body.name != ""  && req.body.type != "" && req.body.country != "" && req.body.region != "" && req.body.description !=""){
+                if (req.body.name != null  && req.body.type != null && req.body.country != null && req.body.region != null  && req.body.description !=null){
+            const poi = this.dao.editPOI(req.body.id,req.body.name, req.body.type,req.body.country,req.body.region,req.body.description,req.body.recommendations)
+            res.status(200).json({success:1});
+                }
+                else{
+                    res.status(400).json({error: req})
+                }
+            } else{
+                res.status(400).json({error: "Blank Field"})
+            }
+        } catch(error){
+            throw error;
+        }
+    }
+    recommendPOI(req,res) {
+        try{
+            const result = this.dao.recommendPOI(req.params.id)
+            //console.log(req.params.id)
+            if (result == true){
+                res.status(200).json({success:1})
+            } else{
+                res.status(404).json({error:'No POI with that ID'})
+            }
+        } catch(error){
+            throw error
+            
+        }
+    }
+    reviewPOI(req,res) {
+        try{
+            if (this.dao.checkPOIID(req.body.poi_id) == true){
+                if (req.body.review != null){
+                    if (req.body.review != ""){
+            const result = this.dao2.reviewPOI(req.body.poi_id,req.body.review)
+            if(result == true){
+            res.status(200).json({success:1})}
             else{
-                res.status(400).json({error: req})
+                res.status(404).json({error:error})
             }
-        } else{
-            res.status(400).json({error: "Blank Field"})
         }
-    } catch(error){
-        throw error;
-    }
-}
-
-editPOI(req,res){
-    try{
-        if (req.body.name != ""  && req.body.type != "" && req.body.country != "" && req.body.region != "" && req.body.description !=""){
-            if (req.body.name != null  && req.body.type != null && req.body.country != null && req.body.region != null  && req.body.description !=null){
-        const poi = this.dao.editPOI(req.body.id,req.body.name, req.body.type,req.body.country,req.body.region,req.body.description,req.body.recommendations)
-        res.status(200).json({success:1});
-            }
-            else{
-                res.status(400).json({error: req})
-            }
-        } else{
-            res.status(400).json({error: "Blank Field"})
         }
-    } catch(error){
-        throw error;
-    }
-}
-recommendPOI(req,res){
-    try{
-        const result = this.dao.recommendPOI(req.params.id)
-        //console.log(req.params.id)
-        if (result == true){
-            res.status(200).json({success:1})
-        } else{
-            res.status(404).json({error:'No POI with that ID'})
         }
-    } catch(error){
-        throw error
-        
-    }
-}
-reviewPOI(req,res){
-    try{
-        if (this.dao.checkPOIID(req.body.poi_id) == true){
-            if (req.body.review != null){
-                if (req.body.review != ""){
-        const result = this.dao2.reviewPOI(req.body.poi_id,req.body.review)
-        if(result == true){
-        res.status(200).json({success:1})}
         else{
-            res.status(404).json({error:error})
+            res.status(404).json({error:"POI ID doesn't exist"})
+        }
+        } catch(error){
+            throw error
         }
     }
-    }
-    }
-    else{
-        res.status(404).json({error:"POI ID doesn't exist"})
-    }
-    } catch(error){
-        throw error
-    }
-}
-
 }
 
 export default PoiController
